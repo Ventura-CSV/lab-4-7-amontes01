7c644681f283b8f17c042bba0759e7477ef4df7d
Anthony Montes <anthony_montes4@my.vcccd.edu>
2024-06-26 09:42:35 -0700

    Lab 4-7

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 def main():
     numbers = []
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    
+    previous_input = int(input("Enter an integer: "))
+    numbers.append(previous_input)
+    while True:
+        current_input = int(input("Enter an integer: "))
+        if current_input > previous_input:
+            break
+        else:
+            numbers.append(current_input)
+            previous_input = current_input
+
+    
 
     ########################################
     # Do not delete the return statement
